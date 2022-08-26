1cc9c0344e6a714dcab8854fcc493e1b5b67e8b2
Thaís Adriane <thais.adrianebc@gmail.com>
2022-08-25 22:48:12 -0300

    mensagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,15 +3,14 @@ let email
 let profissão
 let idade
 let lugarSonho
+let mensagem
 
 nome = "Thaís"
 email = "thais.adrianebc@gmail.com"
 profissão = "Analista Financeiro"
 idade = 35
 lugarSonho = "Gotemburgo"
+mensagem = "Olá, "+ nome +". Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ lugarSonho +". Confirma pra gente alguns dados? A sua idade é "+ idade +", você é "+ profissão +" e o seu email é "+ email +"? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
 
-console.log (nome) 
-console.log (email) 
-console.log (profissão) 
-console.log (idade) 
-console.log (lugarSonho)
+
+console.log (mensagem)
